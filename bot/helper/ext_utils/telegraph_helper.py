798c0c41d706a2f494b25066aa9509103f1323ac
mrkapoor2233x <95880846+mrkapoor2233x@users.noreply.github.com>
2021-12-10 01:40:23 +0530

    Update telegraph_helper.py

--- bot/helper/ext_utils/telegraph_helper.py
+++ bot/helper/ext_utils/telegraph_helper.py
@@ -57,4 +57,4 @@ class TelegraphHelper:
 			return self.edit_page(path, title, content)
 
 
-telegraph=TelegraphHelper('Mirror-Leech-Telegram-Bot', 'https://cyph3rn0ah')
+telegraph=TelegraphHelper('Mirror-Leech-Telegram-Bot', 'https://t.me/cyph3rn0ah')
